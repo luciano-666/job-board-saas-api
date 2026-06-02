c0d914947759da10af86030b86e47f1c55531a5d
Luciano <ptranduc864@gmail.com>
2026-06-03 00:31:39 +0700

    feat(db): Setup PostgreSQL connection

---
+++ src/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from src.config import settings
+
+
+engine = create_async_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
