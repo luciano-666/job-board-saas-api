ada805a2bfb82b9b563459ea3c2ad941c09c43b5
Luciano <ptranduc864@gmail.com>
2026-06-03 17:47:14 +0700

    feat(model): create User model

---
+++ src/auth/models.py
@@ -0,0 +1,32 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String, Boolean, DateTime, func
+
+
+from src.models import Base
+
+
+class User(Base):
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    email: Mapped[str] = mapped_column(
+        String(255), unique=True, nullable=False, index=True
+    )
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    full_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
+    timezone: Mapped[str] = mapped_column(String(50), nullable=True, default="UTC")
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
