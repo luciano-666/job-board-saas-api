4b6875f36beca17a5234771bb1d11e5cd24478fc
Luciano <ptranduc864@gmail.com>
2026-06-04 14:18:58 +0700

    refactor(model): add some necessary fields

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,12 +1,19 @@
 import uuid
 from datetime import datetime
+from enum import Enum as PyEnum
 
 from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import String, Boolean, DateTime, func
+from sqlalchemy import String, Boolean, DateTime, Enum, func
 
 
 from src.models import Base
+
+
+class UserRole(str, PyEnum):
+    admin = "admin"
+    employer = "employer"
+    candidate = "candidate"
 
 
 class User(Base):
@@ -18,9 +25,14 @@ class User(Base):
     )
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     full_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole), nullable=False)
     timezone: Mapped[str] = mapped_column(String(50), nullable=True, default="UTC")
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
     is_superuser: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    last_login_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=func.now()
     )
